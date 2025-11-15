eeba23f15a594058db133cf96247146d76a15b71
SirrPascal <gathungupascal19@gmail.com>
2025-11-15 23:54:34 +0300

    Fix extra space

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,4 +1,5 @@
 weather_condition = input("What's the weather like today? (sunny/rainy/cold): ").strip().lower()
+
 if weather_condition == "sunny":
     print("Wear a t-shirt and sunglasses.")
 elif weather_condition == "rainy":
@@ -6,4 +7,4 @@ elif weather_condition == "rainy":
 elif weather_condition == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
-    print(" Sorry, I don't have recommendations for this weather.")
+    print("Sorry, I don't have recommendations for this weather.")
